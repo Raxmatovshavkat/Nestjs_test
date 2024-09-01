f2e0bc7af11aab426d3a85dde4fb6530fed3e199
shavkat <rshavkat759@gmail.com>
2024-09-01 07:57:49 +0500

    course dto updated

--- src/course/dto/create-course.dto.ts
+++ src/course/dto/create-course.dto.ts
@@ -1,1 +1,28 @@
-export class CreateCourseDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateCourseDto {
+    @ApiProperty({
+        description: 'The title of the course',
+        example: 'Introduction to Programming',
+    })
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @ApiProperty({
+        description: 'A brief description of the course',
+        example: 'This course covers the basics of programming using Python.',
+    })
+    @IsString()
+    @IsNotEmpty()
+    description: string;
+
+    @ApiProperty({
+        description: 'The ID of the user creating the course',
+        example: 'e7b0a9e8-4f2e-4dd5-9fb5-df9f79d0898f',
+    })
+    @IsString()
+    @IsNotEmpty()
+    userId: string;
+}
